267996b75e0ed00fed4ab115ec548a9413199903
Real Mohsin <real@realmohsin.com>
2018-10-25 11:03:35 -0400

    add AppLayout and AppProvider

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import AppLayout from "./AppLayout";
+import { AppProvider } from "./AppProvider";
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <AppLayout>
+        <AppProvider>hello crypto price map</AppProvider>
+      </AppLayout>
+    );
   }
 }
 
