68e336d8172d96c4d1e28aaa77f53485acc57836
Real Mohsin <real@realmohsin.com>
2018-10-26 04:02:25 -0400

    add menubar

--- src/App/index.js
+++ src/App/index.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import AppLayout from "./AppLayout";
 import { AppProvider } from "./AppProvider";
+import AppBar from "./AppBar";
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
-        <AppProvider>hello crypto price map</AppProvider>
+        <AppProvider>
+          <AppBar />
+          hello crypto price map
+        </AppProvider>
       </AppLayout>
     );
   }
